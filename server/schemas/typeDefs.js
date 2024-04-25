8eaead1e2df188644df7766368420f718a707283
Bernardo Alonso <bernardo4430@gmail.com>
2024-04-24 21:23:58 -0500

    made basic typeDefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,42 @@
+const typeDefs = `
+  type User {
+    id: ID!
+    firstName: String!
+    lastName: String!
+    email: String!
+    githubLink: String!
+    projects: [Project!]!
+  }
+
+  type Project {
+    id: ID!
+    name: String!
+    description: String!
+    images: [String!]!
+    contributors: [User!]!
+    comments: [Comment!]!
+  }
+
+  type Comment {
+    id: ID!
+    text: String!
+    user: User!
+    project: Project!
+  }
+
+  type Query {
+    projects: [Project!]!
+    project(id: ID!): Project!
+    comments(projectId: ID!): [Comment!]!
+  }
+
+  type Mutation {
+    signup(firstName: String!, lastName: String!, email: String!, githubLink: String!): User!
+    login(email: String!): User!
+    logout: Boolean!
+    createProject(name: String!, description: String!, images: [String!]!): Project!
+    addComment(projectId: ID!, text: String!): Comment!
+  }
+`;
+
+module.exports = typeDefs;
