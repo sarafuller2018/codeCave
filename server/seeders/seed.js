fa2ad9fbef74d52f88210d1288048a4d2c671ad9
Sara Fuller <149854566+sarafuller2018@users.noreply.github.com>
2024-04-26 13:44:32 -0500

    began front-end auth

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,22 +1,22 @@
 // set up sourced from in class activities 
-const db = require('../config/connection');
-const { User, Project, Comment } = require('../models');
-const cleanDB = require('./cleanDB');
+// const db = require('../config/connection');
+// const { User, Project, Comment } = require('../models');
+// const cleanDB = require('./cleanDB');
 
-const userData = require('./userData.json');
-const projectData = require('./projectData.json');
-const commentData = require('./commentData.json');
+// const userData = require('./userData.json');
+// const projectData = require('./projectData.json');
+// const commentData = require('./commentData.json');
 
-db.once('open', async () => {
-  // clean database
-  await cleanDB("User", "users");
-  await cleanDB("Project", "projects");
-  await cleanDB("Comment", "comments");
+// db.once('open', async () => {
+//   // clean database
+//   await cleanDB("User", "users");
+//   await cleanDB("Project", "projects");
+//   await cleanDB("Comment", "comments");
 
-  await User.create(userData);
-  await Project.create(projectData);
-  await Comment.create(commentData);
+//   await User.create(userData);
+//   await Project.create(projectData);
+//   await Comment.create(commentData);
 
-  console.log('seed finished!');
-  process.exit(0);
-});
+//   console.log('seed finished!');
+//   process.exit(0);
+// });
