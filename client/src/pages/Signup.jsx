d4def92aacf91b2d010579f49fff1d599f9bf560
Sara Fuller <149854566+sarafuller2018@users.noreply.github.com>
2024-04-29 20:02:01 -0500

    trying to merge

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,3 +1,102 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import { useMutation } from '@apollo/client';
+import { ADD_USER } from '../utils/mutations';
+
+const Signup = () => {
+    const [formState, setFormState] = useState({
+      username: '',
+      email: '',
+      password: '',
+    });
+    const [addUser, { error, data }] = useMutation(ADD_USER);
+  
+    const handleChange = (event) => {
+      const { name, value } = event.target;
+  
+      setFormState({
+        ...formState,
+        [name]: value,
+      });
+    };
+  
+    const handleFormSubmit = async (event) => {
+      event.preventDefault();
+      console.log(formState);
+  
+      try {
+        const { data } = await addUser({
+          variables: { ...formState },
+        });
+  
+        Auth.login(data.addUser.token);
+      } catch (e) {
+        console.error(e);
+      }
+    };
+
+    return (
+        <main>
+          <div>
+            <div className="signup-container">
+              <h4>Sign Up</h4>
+              <div>
+                {data ? (
+                  <p>
+                    Success! You may now head{' '}
+                    <Link to="/">back to the homepage.</Link>
+                  </p>
+                ) : (
+                  <form onSubmit={handleFormSubmit}>
+                    <input
+                      className="form-input"
+                      placeholder="Your username"
+                      name="username"
+                      type="text"
+                      value={formState.name}
+                      onChange={handleChange}
+                    />
+                    <input
+                      className="form-input"
+                      placeholder="Your email"
+                      name="email"
+                      type="email"
+                      value={formState.email}
+                      onChange={handleChange}
+                    />
+                    <input
+                      className="form-input"
+                      placeholder="******"
+                      name="password"
+                      type="password"
+                      value={formState.password}
+                      onChange={handleChange}
+                    />
+                    <button
+                      className="submit-btn"
+                      style={{ cursor: 'pointer' }}
+                      type="submit"
+                    >
+                      Submit
+                    </button>
+                  </form>
+                )}
+    
+                {error && (
+                  <div>
+                    {error.message}
+                  </div>
+                )}
+              </div>
+            </div>
+          </div>
+        </main>
+      );
+    };
+    
+    export default Signup;
+
 // import { useState } from 'react';
 // import { Link } from 'react-router-dom';
 // import { useMutation } from '@apollo/client';
