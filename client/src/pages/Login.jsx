adff9bcc557b4fd18032e643e1c858c3309c7522
Efren Leal <efrenleal19@gmail.com>
2024-04-29 19:00:42 -0500

    changed login components to page file

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,1 +1,78 @@
 import Auth from '../utils/auth';
+
+// login page 
+const Login = () => {
+    return (
+        <section>
+            <div className="logo-div">
+                <img className="codecave-login-logo" src="./Images/codeCave(login-logo).svg" />
+            </div>
+            <div className="form-div">
+                <form className="form-card">
+                    <div className="login-text-div">
+                        <p className="login-text">Login</p>
+                    </div>
+                    <div className="input-div">
+                        <div className="email-div">
+                            <input className="login-input" type="email" placeholder="E-mail" 
+                                name="email" onChange />
+                            <label className="form-label" ></label>
+                        </div>
+                        <div className="password-div">
+                            <input className="login-input" type="text" placeholder="Password"
+                                name="userName" onChange />
+                            <label className="form-label" ></label>
+                        </div>
+                        <div className="login-btn-div">
+                            <button className="login-btn">Login</button>
+                        </div>
+                        <div className="or-div">
+                            <p className="or">or</p>
+                        </div>
+                        <div className="signup-btn-div">
+                            <button className="signup-btn">Sign Up</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div className="signup-form-div">
+                <form className="form-card" id="signup-form-card">
+                    <div className="login-text-div">
+                        <p className="login-text">Sign Up</p>
+                    </div>
+                    <div className="name-div">
+                            <input className="name-input" type="text" placeholder="First Name" 
+                                name="name" onChange />
+                            <label className="form-label" ></label>
+                            <input className="name-input" type="text" placeholder="Last Name" 
+                                name="name" onChange />
+                            <label className="form-label" ></label>
+                        </div>
+                    <div className="input-div">
+                        <div className="email-div">
+                            <input className="login-input" type="email" placeholder="E-mail" 
+                                name="email" onChange />
+                            <label className="form-label" ></label>
+                        </div>
+                        <div className="password-div">
+                            <input className="login-input" type="text" placeholder="Set Password"
+                                name="userName" onChange />
+                            <label className="form-label" ></label>
+                        </div>
+                        <div className="login-btn-div">
+                            <button className="login-btn">Sign Up</button>
+                        </div>
+                        <div className="or-div">
+                            <p className="or">or</p>
+                        </div>
+                        <div className="signup-btn-div">
+                            <button className="signup-btn">Login</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </section>
+    )
+}
+
+export default Login;
