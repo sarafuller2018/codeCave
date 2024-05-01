b049f84088c064532630fb46bcb460581b1c78ab
Bernardo Alonso <bernardo4430@gmail.com>
2024-05-01 00:42:07 -0500

    adding difrent email service

--- client/src/pages/SingleProjectDetails.jsx
+++ client/src/pages/SingleProjectDetails.jsx
@@ -1,13 +1,12 @@
 import { useQuery } from '@apollo/client';
 import { useParams } from 'react-router-dom';
 import { QUERY_SINGLE_PROJECT } from '../utils/queries';
-import Header from '../components/Header/Header';
-import { sendEmail } from '../../../server/utils/emailUtils';
 
-const SingleProjectDetails = ({ user }) => {
+const SingleProjectDetails = () => {
     const { projectId } = useParams();
+
     const { loading, data } = useQuery(QUERY_SINGLE_PROJECT, {
-        variables: { projectId: projectId },
+        variables: { projectId: projectId},
     });
 
     const project = data?.project || {};
@@ -15,51 +14,26 @@ const SingleProjectDetails = ({ user }) => {
     if (loading) {
         return <div>Loading...</div>;
     }
-
-    const handleAskToContribute = async () => {
-        // if (!user || !project || !project.user || !project.user.email) {
-        //     console.error('Invalid user or project data.');
-        //     return;
-        // }
-    
-        const from = "test@email.com";
-        const to = "bernardo4430@gmail.com";
-        const subject = 'Someone wants to help!';
-        const text = ` wants to help on your project.`;
-    
-        try {
-            await sendEmail(from, to, subject, text);
-            alert('Email sent successfully');
-        } catch (error) {
-            console.error('Error sending email:', error);
-            alert('Failed to send email');
-        }
-    };
     return (
-        <>
-            <Header />
-            <div className="project-card-div">
-                    <div className="project-card" key={project._id}>
-                        <div className="project-title-div">
-                            <p className="project-title">{project.name}</p>
-                        </div>
-                        <div className="project-description-div">
-                            <p className="project-description">{project.description}</p>
-                        </div>
-                        <div className="placeholder-img-div">
-                            <img className="placeholder-img" src={project.image} />
-                        </div>
-                        <div >
-                            <p className="time-stamp">{project.createdAt}</p>
-                        </div>
-                <div className="contribute-btn-div">
-                    <button className="contribute-btn" onClick={handleAskToContribute}>
-                        Ask To Contribute
-                    </button>
-                </div>
+        <div className="project-card-div">
+        <div className="project-card" key={project._id}>
+            <div className="project-title-div">
+                <p className="project-title">{project.name}</p>
+            </div>
+            <div className="project-description-div">
+                <p className="project-description">{project.description}</p>
+            </div>
+            <div className="placeholder-img-div">
+                <img className="placeholder-img" src={project.image} />
+            </div>
+            <div >
+                <p className="time-stamp">{project.createdAt}</p>
+            </div>
+            <div className="contribute-btn-div">
+                <button className="contribute-btn">Ask To Contribute</button>
             </div>
         </div>
-        </>
+    </div>
     );
 };
 
