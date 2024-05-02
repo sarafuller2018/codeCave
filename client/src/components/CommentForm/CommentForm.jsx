1cd9a32a9ec1a04bc6dd06920389fe8c542c4ea8
unknown <whritenourc@outlook.com>
2024-05-01 21:31:56 -0500

    stash

--- client/src/components/CommentForm/CommentForm.jsx
+++ client/src/components/CommentForm/CommentForm.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const CommentForm = ({ isOpen, toggleForm}) => {
+const CommentForm = ({ isOpen, toggleForm }) => {
     const [formState, setFormState] = useState({
         commentText: '',
     });
@@ -22,18 +22,18 @@ const CommentForm = ({ isOpen, toggleForm}) => {
                 </div>
                 <div>
                     <input className="comment-text-input" type="text" placeholder="Your comments here"
-                    name="comment-text" value={formState.text}
-                    onChange={handleChange} />
+                        name="comment-text" value={formState.text}
+                        onChange={handleChange} />
                 </div>
                 <div className="addComment-btn-div">
-                                <button
-                                    className="addComment-btn"
-                                    style={{ cursor: 'pointer' }}
-                                    type="submit"
-                                >
-                                    Submit Comment
-                                </button>
-                            </div>
+                    <button
+                        className="addComment-btn"
+                        style={{ cursor: 'pointer' }}
+                        type="submit"
+                    >
+                        Submit Comment
+                    </button>
+                </div>
             </form>
         </div>
     );
